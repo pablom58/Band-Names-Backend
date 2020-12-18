56027a36b2b8f2624f49cfef2292aeb41a148897
Pablo Villamizar <pablomvillamizar@gmail.com>
2020-12-17 21:10:51 -0400

    BandNames

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
+app.use(express.static(path.join(__dirname,'../build')))
 app.use(cors())
 
 module.exports = app
